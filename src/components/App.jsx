133c1342332bca1ff86d66faa1c36d044dc2d525
AmroAli101 <123amroraed@gmail.com>
2025-01-30 11:14:59 +0200

    Update App.jsx, CreateArea.jsx, and Note.jsx components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleAddNote = () => {
+    const newNote = { id: Date.now(), title, content };
+    setNotes([...notes, newNote]);
+    setTitle("");
+    setContent("");
+  };
+
+ 
+  const handleDeleteNote = (id) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea
+        title={title}
+        content={content}
+        setTitle={setTitle}
+        setContent={setContent}
+        handleAddNote={handleAddNote}
+        disableButton={!title || !content}
+      />
+      {notes.map((note) => (
+        <Note 
+          key={note.id} 
+          id={note.id} 
+          title={note.title} 
+          content={note.content} 
+          onDelete={handleDeleteNote} 
+        />
+      ))}
       <Footer />
     </div>
   );
