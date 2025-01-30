5d4b5e84bd7ffce210dab93064ca5858e00f79fe
AmroAli101 <123amroraed@gmail.com>
2025-01-30 11:14:59 +0200

    Update App.jsx, CreateArea.jsx, and Note.jsx components

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea({ title, content, setTitle, setContent, handleAddNote, disableButton }) {
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    handleAddNote();
+  };
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="submit" disabled={disableButton}>
+          Add
+        </button>
       </form>
     </div>
   );
